57cca8bd2f8f470bd3bf6c576a5d9e73435eb7e9
Abastv <abastv43@gmail.com>
2025-09-28 13:53:33 +0500

    Delete spam.py

--- spam.py
+++
@@ -1,9 +0,0 @@
-from flask import Request, Response
-
-def handler(request: Request) -> Response:
-    word = request.args.get("word", "SPAM")
-    count = int(request.args.get("count", 1))
-    if count > 50:
-        count = 50
-    result = "\n".join([word] * count)
-    return Response(result, mimetype="text/plain")
